3f34867fd0a0dea140b7e3b6e2b1135314eddd83
victor-lima-142 <victoreboredo@gmail.com>
2022-05-11 22:13:53 -0300

    Adding 'Who we are?' Section

--- React/landing-page/src/Components/WhoWeAre/index.jsx
+++ React/landing-page/src/Components/WhoWeAre/index.jsx
@@ -1,9 +1,24 @@
-import { Container } from "react-bootstrap";
+import { Card, Col, Container, Image, Row } from "react-bootstrap";
+import './WhoWeAre.css'
 
 export default function WhoWeAre() {
     return (
-        <Container fluid={true} className={`wrapper-container`}>
-            
+        <Container fluid className={'who-we-are-container'}>
+            <Row className={`align-items-center`}>
+                <Col md={6} sm={12} xs={12} lg={6}>
+                    <Image src={require('../../assets/panels-figure.png')} className='panels-figure'/>
+                </Col>
+                <Col md={6} sm={12} xs={12} lg={6}>
+                    <Card className={`border-0 rounded-0 card-who-we-are`}>
+                        <div className={`border-0 rounded-0 text-center`}><Card.Title>Who We Are</Card.Title></div>
+                        <Card.Body className='who-we-are-text-section bg-gradient'>
+                            A Infinitus Solar é uma empresa habilitada e qualificada para desenvolver projetos, instalações e manutenções de sistemas de energia solar.
+                            A produção da energia solar alcança ótimos benefícios tanto para a natureza quanto para famílias que pagam tão caro pela energia elétrica fornecida pelas concessionárias.
+                            Entendendo essa realidade a Infinitus Solar oferece a solução: vendemos e instalamos sistemas de energia solar fotovoltaica que gera energia limpa e ilimitada.
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </Container>
     );
 }
