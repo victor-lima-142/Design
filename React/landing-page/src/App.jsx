87a37dce3fdd1b8a55116bc77fad70535d16969f
victor-lima-142 <victoreboredo@gmail.com>
2022-05-12 00:34:26 -0300

    Organizating tree of website components

--- React/landing-page/src/App.jsx
+++ React/landing-page/src/App.jsx
@@ -4,6 +4,7 @@ import Banner from './Components/Banner'
 import CardBanner from "./Components/CardBanner";
 import WhoWeAre from "./Components/WhoWeAre";
 import CarouselSection from "./Components/CarouselSection";
+import FormContact from "./Components/FormContact";
 
 export default function App() {
     return (
@@ -13,6 +14,7 @@ export default function App() {
             <CardBanner />
             <WhoWeAre />
             <CarouselSection />
+            <FormContact />
             <br />
             <br />
             <br />
